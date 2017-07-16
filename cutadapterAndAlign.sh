aecd0e324fb6d0b989927c8cf838f0a689d5c789
Shang Gao <sgao30@uic.edu>
2017-07-16 10:06:58 -0500

    Update and rename all.sh to cutadapterAndAlign.sh

--- cutadapterAndAlign.sh
+++ cutadapterAndAlign.sh
@@ -13,19 +13,3 @@ do
 	~/Tools/SHRiMP_2_2_3/bin/gmapper-ls -L smallRNA.final-ls $i.trimmed.fasta -n 1 --max-alignments 0 -U --local --strata > $i/$i.smallRNA.best.mapping.strata.sam
 done
 
-
-
-
-
-
-
-
-
-
-##~/Tools/SHRiMP_2_2_3/bin/gmapper-ls -L piRNA.hg19.uniq-ls 50188.trimmed.fasta --max-alignments 0 -n 1 -U --local --strata > 50188.piRNA.best.mapping.strata.sam
-##~/Tools/SHRiMP_2_2_3/bin/gmapper-ls -L tRNA.hg19.final-ls 50188.trimmed.fasta -n 1 --max-alignments 0 -U --local --strata > 50188.tRNA.best.mapping.strata.sam
-##~/Tools/SHRiMP_2_2_3/bin/gmapper-ls -L rRNA.hg19.final-ls 50188.trimmed.fasta -n 1 --max-alignments 0 -U --local --strata > 50188.rRNA.best.mapping.strata.sam
-##~/Tools/SHRiMP_2_2_3/bin/gmapper-ls -L snoRNA.hg19.final-ls 50188.trimmed.fasta -n 1 --max-alignments 0 -U --local --strata > 50188.snoRNA.best.mapping.strata.sam
-##~/Tools/SHRiMP_2_2_3/bin/gmapper-ls -L snRNA.hg19.final-ls 50188.trimmed.fasta -n 1 --max-alignments 0 -U --local --strata > 50188.snRNA.best.mapping.strata.sam
-##~/Tools/SHRiMP_2_2_3/bin/gmapper-ls -L smallRNA.final-ls 50188.trimmed.fasta -n 1 --max-alignments 0 -U --local --strata > 50188.smallRNA.best.mapping.strata.sam
-
